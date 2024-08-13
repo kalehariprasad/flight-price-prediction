6d1bebaed6d897b9a539d1f2bb3fe9f010a554cc
Hari prasad <hariprasad9693@gmail.com>
2024-08-13 10:49:22 +0530

    intial commit with cookicutter template

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='flight price predioction ml Project',
+    author='hari prasad',
+    license='',
+)
